bb0a0f733b7417f8da02248dd96c2d1e4f3c4b89
Everett Wilson <everettwilson@pop-os.localdomain>
2019-12-03 08:38:14 -0600

    Switching to laptop

--- src/ui.rs
+++ src/ui.rs
@@ -1,11 +1,6 @@
 use super::collection;
 
-extern crate gio;
-extern crate gtk;
-use gio::prelude::*;
-use gtk::prelude::*;
-
-use gtk::{Application, ApplicationWindow, Button};
+use orbtk::prelude::*;
 
 struct UISettings {
     width: i32,
@@ -17,27 +12,83 @@ const SETTINGS: UISettings = UISettings {
     height: 480,
 };
 
+#[derive(Debug, Copy, Clone)]
+enum Action {
+    ChooseDeck(Entity),
+}
+
+#[derive(Default)]
+pub struct MainViewState {
+    action: Option<Action>,
+}
+
+impl State for MainViewState {
+    fn update(&self, ctx: &mut Context) {
+
+    }
+}
+
+type StringVec = Vec<String>;
+
+widget!(MainView<MainViewState> {
+    text: String,
+    stuff: StringVec
+});
+
+impl Template for MainView {
+    fn template(self, _id: Entity, ctx: &mut BuildContext) -> Self {
+        self.name("MainView")
+            .width(SETTINGS.width as f64)
+            .height(SETTINGS.height as f64)
+            .child(
+                Grid::create()
+                    .rows(Rows::create().row(72.0).row("*").build())
+                    .rows(Rows::create().row(100.0).row("|").build())
+                    .child(
+                        Container::create()
+                            .attach(Grid::row(0))
+                            .child(
+                                TextBlock::create()
+                                    .width(10.0)
+                                    .height(10.0)
+                                    .text("Hi")
+                                    .build(ctx),
+                            )
+                            .build(ctx),
+                    )
+                    .child(
+                        ScrollViewer::create()
+                            .attach(Grid::row(1))
+                            .child(
+                                ListView::create()
+                                    .items_builder(
+                                        move |bc, index| {
+                                            let text = "BLAH";
+
+                                            TextBlock::create()
+                                                .text(text)
+                                                .build(bc)
+                                        }
+                                    )
+                                    .build(ctx)
+                            )
+                            .build(ctx),
+                    )
+                    .selector(Selector::from("container").class("title"))
+                    .build(ctx)
+            )
+    }
+}
+
 pub fn render(collection: collection::Collection) {
-    let application =
-        Application::new(Some("com.github.gtk-rs.examples.basic"), Default::default())
-            .expect("failed to initialize GTK application");
-
-    application.connect_activate(move |app| {
-        let window = ApplicationWindow::new(app);
-        window.set_title("First GTK+ Program");
-        window.set_default_size(SETTINGS.width, SETTINGS.height);
-
-        for deck in &collection.decks {
-            let button = Button::new_with_label(&deck.title);
-            button.connect_clicked(|_| {
-                println!("Clicked deck with name {}", "blah");
-                deck.card_by_index(1);
-            });
-            window.add(&button);
-        }
-
-        window.show_all();
-    });
-
-    application.run(&[]);
+    Application::new()
+        .window(|ctx| {
+            Window::create()
+                .title("OrbTk - minimal example")
+                .position((200.0, 100.0))
+                .size(SETTINGS.width as f64, SETTINGS.height as f64)
+                .child(MainView::create().build(ctx))
+                .build(ctx)
+        })
+        .run();
 }
